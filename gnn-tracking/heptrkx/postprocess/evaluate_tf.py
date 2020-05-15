405b260d7676721b1e7a8cd4f979b02725c2e699
Javier Duarte <jduarte@ucsd.edu>
2020-05-14 18:29:59 -0700

    get model weights and biases

--- gnn-tracking/heptrkx/postprocess/evaluate_tf.py
+++ gnn-tracking/heptrkx/postprocess/evaluate_tf.py
@@ -37,6 +37,7 @@ def create_evaluator(config_name, iteration, input_ckpt=None):
     tf.compat.v1.reset_default_graph()
     model = get_model(config['model_name'])
 
+
     input_graphs, target_graphs = generate_input_target(n_graphs)
     input_ph  = utils_tf.placeholders_from_data_dicts(input_graphs, force_dynamic_num_graphs=True)
     target_ph = utils_tf.placeholders_from_data_dicts(target_graphs, force_dynamic_num_graphs=True)
@@ -70,4 +71,100 @@ def create_evaluator(config_name, iteration, input_ckpt=None):
             use_digraph=use_digraph,
             bidirection=bidirection)
 
-    return evaluator
+    return evaluator, model, sess
+
+
+def create_profiler(config_name, iteration, input_ckpt=None):
+    """
+    @config: configuration for train_nx_graph
+    """
+    # load configuration file
+    all_config = load_yaml(config_name)
+    config = all_config['segment_training']
+    config_tr = config['parameters']
+
+    batch_size = n_graphs   = config_tr['batch_size']   # need optimization
+    num_processing_steps_tr = config_tr['n_iters']      ## level of message-passing
+    prod_name = config['prod_name']
+    if input_ckpt is None:
+        input_ckpt = os.path.join(config['output_dir'], prod_name)
+
+    # generate inputs
+    generate_input_target = inputs_generator(all_config['make_graph']['out_graph'], n_train_fraction=0.8)
+
+    # build TF graph
+    tf.compat.v1.reset_default_graph()
+    model = get_model(config['model_name'])
+
+    input_graphs, target_graphs = generate_input_target(n_graphs)
+    input_ph  = utils_tf.placeholders_from_data_dicts(input_graphs, force_dynamic_num_graphs=True)
+    target_ph = utils_tf.placeholders_from_data_dicts(target_graphs, force_dynamic_num_graphs=True)
+
+    output_ops_tr = model(input_ph, num_processing_steps_tr)
+
+    try:
+        sess.close()
+    except NameError:
+        pass
+
+    sess = tf.compat.v1.Session()
+    saver = tf.compat.v1.train.Saver()
+    if iteration < 0:
+        saver.restore(sess, tf.train.latest_checkpoint(input_ckpt))
+    else:
+        saver.restore(sess, os.path.join(input_ckpt, ckpt_name.format(iteration)))
+
+    def profiler(input_graphs, target_graphs, use_digraph=False, bidirection=False):
+        """
+        input is graph tuples, sizes should match batch_size
+        """
+
+        print(model._encoder._network._edge_model._build_function)
+        print(model._encoder._network._node_model._build_function)
+
+        for layer in model._core._edge_block._edge_model._layers:
+            print(layer._output_sizes)
+        for layer in model._core._node_block._node_model._layers:
+            print(layer._output_sizes)
+
+        print(model._decoder._edge_model._build_function)
+        print(model._output_transform._edge_model._build_function)
+
+        print(input_graphs.nodes.shape)
+        print(input_graphs.edges.shape)
+        print(input_graphs.receivers.shape)
+        print(input_graphs.senders.shape)
+        print(input_graphs.n_node.shape)
+        print(input_graphs.n_edge.shape)        
+        print(input_graphs.globals.shape)
+        flops = 1
+
+        #feed_dict = {input_ph: input_graphs, target_ph: target_graphs}
+        #pred_node_names = []
+        #for gtuple in output_ops_tr:
+        #    pred_node_names.append(gtuple.nodes.name[:-2]) # remove ':0'
+        #    pred_node_names.append(gtuple.edges.name[:-2]) # remove ':0'
+        #    pred_node_names.append(gtuple.globals.name[:-2]) # remove ':0'
+        #    pred_node_names.append(gtuple.receivers.name[:-2]) # remove ':0'
+        #    pred_node_names.append(gtuple.senders.name[:-2]) # remove ':0'
+        #    pred_node_names.append(gtuple.n_node.name[:-2]) # remove ':0'
+        #    pred_node_names.append(gtuple.n_edge.name[:-2]) # remove ':0'
+        #for gtuple in target_ph:
+        #    pred_node_names.append(gtuple.name[:-2]) # remove ':0'
+        #from tensorflow.python.framework import graph_util
+        #constant_graph_def = tf.compat.v1.graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), pred_node_names)
+        #constant_graph = tf.Graph()
+        #with constant_graph.as_default():
+        #    tf.import_graph_def(constant_graph_def,name='') 
+        #    op = constant_graph.get_operations()
+        #    run_metadata = tf.compat.v1.RunMetadata()
+        #    predictions = sess.run({
+        #        "outputs": output_ops_tr,
+        #       "target": target_ph
+        #    }, feed_dict=feed_dict,
+        #    options=tf.compat.v1.RunOptions(trace_level=tf.compat.v1.RunOptions.FULL_TRACE))
+        #flops = tf.profiler.profile(constant_graph, options = tf.profiler.ProfileOptionBuilder.float_operation(),run_meta=run_metadata)
+
+        return flops
+
+    return profiler
