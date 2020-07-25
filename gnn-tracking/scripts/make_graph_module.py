4aeb6ef47aad35cb372b30e38853551a040b6a05
Javier M. Duarte <jduarte@ucsd.edu>
2020-07-24 18:53:40 -0700

    make flexible

--- gnn-tracking/scripts/make_graph_module.py
+++ gnn-tracking/scripts/make_graph_module.py
@@ -57,33 +57,135 @@ file_name = file_names[0]
 with np.load(file_name) as f:
     input_graphs.append(dict(f.items()))
 
+
+# consider only first MAX_NODES nodes
+MAX_NODES = 100
+input_graphs[0]['nodes'] = input_graphs[0]['nodes'][0:MAX_NODES]
+input_graphs[0]['n_node'] = np.asarray(input_graphs[0]['nodes'].shape[0])
+# cut edges that don't conncet first MAX_NODES nodes
+mask = (input_graphs[0]['receivers'] < MAX_NODES) & (input_graphs[0]['senders'] < MAX_NODES)
+input_graphs[0]['receivers'] = input_graphs[0]['receivers'][mask]
+input_graphs[0]['senders'] = input_graphs[0]['senders'][mask]
+input_graphs[0]['edges'] = input_graphs[0]['edges'][mask]
+input_graphs[0]['n_edge'] = np.asarray(input_graphs[0]['edges'].shape[0])
+input_graphs[0]['globals'] = input_graphs[0]['globals']
+
 with np.load(file_name.replace("INPUT", "TARGET")) as f:
     target_graphs.append(dict(f.items()))
 
-print("input_graphs:")
-print(input_graphs)
-
 tf.reset_default_graph()
+# make latent dim smaller (32)
+LATENT = 32
+encoder = gn.modules.GraphIndependent(
+    edge_model_fn=lambda: snt.Sequential([snt.nets.MLP([LATENT, LATENT], activation=tf.nn.relu, activate_final=True)]),
+    node_model_fn=lambda: snt.Sequential([snt.nets.MLP([LATENT, LATENT], activation=tf.nn.relu, activate_final=True)]),
+    global_model_fn=None)
 
+core = gn.modules.InteractionNetwork(
+    edge_model_fn=lambda: snt.Sequential([snt.nets.MLP([LATENT, LATENT], activation=tf.nn.relu, activate_final=True)]),
+    node_model_fn=lambda: snt.Sequential([snt.nets.MLP([LATENT, LATENT], activation=tf.nn.relu, activate_final=True)]),
+    reducer=tf.unsorted_segment_sum)
 
-graph_net_module = gn.modules.InteractionNetwork(
-    edge_model_fn=lambda: snt.nets.MLP([32]),
-    node_model_fn=lambda: snt.nets.MLP([32]))
+decoder = gn.modules.GraphIndependent(
+    edge_model_fn=lambda: snt.Sequential([snt.nets.MLP([LATENT, LATENT, LATENT, 1], activation=tf.nn.relu, activate_final=False), 
+                                          tf.sigmoid]),
+    node_model_fn=None,
+    global_model_fn=None)
 
 print("graph_net_module instantiated")
 print()
 
 # Create a placeholder using the first graph in the list as template.
-graphs_tuple_ph = utils_tf.placeholders_from_data_dicts(input_graphs[0:1])
-tf_output_graphs = graph_net_module(graphs_tuple_ph)
+graphs_tuple_ph = utils_tf.placeholders_from_data_dicts(input_graphs)
+tf_output_graphs = decoder(core(encoder(graphs_tuple_ph)))
+
+encoder_edge_biases = []
+encoder_edge_weights = []
+encoder_node_biases = []
+encoder_node_weights = []
+
+core_edge_weights = []
+core_edge_biases = []
+core_node_weights = []
+core_node_biases = []
+
+decoder_edge_biases = []
+decoder_edge_weights = []
 
 with tf.Session() as sess:
+    # randomly initialize the weights
     init = tf.global_variables_initializer()
     sess.run(init)
-    feed_dict = utils_tf.get_feed_dict(graphs_tuple_ph, utils_np.data_dicts_to_graphs_tuple(input_graphs[0:1]))
+
+    # get the output predictions
+    feed_dict = utils_tf.get_feed_dict(graphs_tuple_ph, utils_np.data_dicts_to_graphs_tuple(input_graphs))
     output_graphs = sess.run(tf_output_graphs, feed_dict=feed_dict)
 
-print("output_graphs")
+    # save the weights of the model
+    for v in encoder._edge_model.trainable_variables:
+        if 'b:0' in v.name:
+            encoder_edge_biases.append(v.eval(session=sess))
+        elif 'w:0' in v.name:
+            encoder_edge_weights.append(v.eval(session=sess))
+    for v in encoder._node_model.trainable_variables:
+        if 'b:0' in v.name:
+            encoder_node_biases.append(v.eval(session=sess))
+        elif 'w:0' in v.name:
+            encoder_node_weights.append(v.eval(session=sess))
+
+    for v in core._edge_block._edge_model.trainable_variables:
+        if 'b:0' in v.name:
+            core_edge_biases.append(v.eval(session=sess))
+        elif 'w:0' in v.name:
+            core_edge_weights.append(v.eval(session=sess))
+    for v in core._node_block._node_model.trainable_variables:
+        if 'b:0' in v.name:
+            core_node_biases.append(v.eval(session=sess))
+        elif 'w:0' in v.name:
+            core_node_weights.append(v.eval(session=sess))
+
+    for v in decoder._edge_model.trainable_variables:
+        if 'b:0' in v.name:
+            decoder_edge_biases.append(v.eval(session=sess))
+        elif 'w:0' in v.name:
+            decoder_edge_weights.append(v.eval(session=sess))
+
+
+output_graphs = utils_np.graphs_tuple_to_data_dicts(output_graphs)
+
+print("input_graphs:")
+print(input_graphs)
+print("output_graphs:")
 print(output_graphs)
 
+nodes = input_graphs[0]['nodes']
+edges = input_graphs[0]['edges']
+receivers = input_graphs[0]['receivers']
+senders = input_graphs[0]['senders']
+predict = output_graphs[0]['edges']
 
+# save the input and output graphs for use in test bench
+np.savetxt('tb_input_node_features.dat',nodes.reshape((1,-1)),delimiter=', ')
+np.savetxt('tb_input_edge_features.dat',edges.reshape((1,-1)),delimiter=', ')
+np.savetxt('tb_receivers.dat',receivers.reshape((1,-1)),delimiter=', ',fmt='%d')
+np.savetxt('tb_senders.dat',senders.reshape((1,-1)),delimiter=', ',fmt='%d')
+np.savetxt('tb_output_edge_predictions.dat',predict.reshape((1,-1)),delimiter=', ')
+
+import hls4ml
+# save the weights for use in hls4ml
+os.makedirs('./firmware/weights',exist_ok=True)
+for i, (w, b) in enumerate(zip(encoder_edge_weights, encoder_edge_biases)):
+    var = hls4ml.model.hls_model.WeightVariable('encoder_w%i'%i, type_name='ap_fixed<16,6>', precision='<16,6>', data=w)
+    hls4ml.writer.VivadoWriter.print_array_to_cpp(None,var,'./')
+    var = hls4ml.model.hls_model.WeightVariable('encoder_b%i'%i, type_name='ap_fixed<16,6>', precision='<16,6>', data=b)
+    hls4ml.writer.VivadoWriter.print_array_to_cpp(None,var,'./')
+for i, (w, b) in enumerate(zip(core_edge_weights, core_edge_biases)):
+    var = hls4ml.model.hls_model.WeightVariable('core_w%i'%i, type_name='ap_fixed<16,6>', precision='<16,6>', data=w)
+    hls4ml.writer.VivadoWriter.print_array_to_cpp(None,var,'./')
+    var = hls4ml.model.hls_model.WeightVariable('core_b%i'%i, type_name='ap_fixed<16,6>', precision='<16,6>', data=b)
+    hls4ml.writer.VivadoWriter.print_array_to_cpp(None,var,'./')
+for i, (w, b) in enumerate(zip(decoder_edge_weights, decoder_edge_biases)):
+    var = hls4ml.model.hls_model.WeightVariable('decoder_w%i'%i, type_name='ap_fixed<16,6>', precision='<16,6>', data=w)
+    hls4ml.writer.VivadoWriter.print_array_to_cpp(None,var,'./')
+    var = hls4ml.model.hls_model.WeightVariable('decoder_b%i'%i, type_name='ap_fixed<16,6>', precision='<16,6>', data=b)
+    hls4ml.writer.VivadoWriter.print_array_to_cpp(None,var,'./')
