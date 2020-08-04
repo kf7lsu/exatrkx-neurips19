d4ee83d091e34d61a087ddca178bc77531b5e946
Javier M. Duarte <jduarte@ucsd.edu>
2020-08-04 10:50:52 -0700

    update

--- gnn-tracking/scripts/make_graph_module.py
+++ gnn-tracking/scripts/make_graph_module.py
@@ -5,7 +5,6 @@ import numpy as np
 import setGPU
 
 import tensorflow as tf
-#tf.random.set_seed(42)
 tf.compat.v1.set_random_seed(42)
 from graph_nets import utils_np, utils_tf
 
@@ -59,25 +58,41 @@ file_name = file_names[0]
 with np.load(file_name) as f:
     input_graphs.append(dict(f.items()))
 
-
-# consider only first MAX_NODES nodes
-MAX_NODES = 100
-input_graphs[0]['nodes'] = input_graphs[0]['nodes'][0:MAX_NODES]
+# consider only MAX_NODES nodes (randomly selected)
+MAX_NODES = 130
+X = input_graphs[0]['nodes']
+Xmask = (X[:,0] > 0.4) & (np.abs(X[:,1]) < 0.2) # take 1/5 of 1/8 phi slice, and last 4 layers
+X = X[Xmask]
+#import matplotlib.pyplot as plt
+#plt.figure()
+#plt.hist(X[:,0],bins=np.linspace(0, 1.1, 101))
+#plt.savefig('X.png')
+#plt.figure()
+#plt.hist(X[:,1],bins=np.linspace(-1.1, 1.1, 101))
+#plt.savefig('Y.png')
+index = np.sort(np.random.choice(X.shape[0], MAX_NODES, replace=False))
+input_graphs[0]['nodes'] = X[index]
 input_graphs[0]['n_node'] = np.asarray(input_graphs[0]['nodes'].shape[0])
-# cut edges that don't conncet first MAX_NODES nodes
-mask = (input_graphs[0]['receivers'] < MAX_NODES) & (input_graphs[0]['senders'] < MAX_NODES)
-input_graphs[0]['receivers'] = input_graphs[0]['receivers'][mask]
-input_graphs[0]['senders'] = input_graphs[0]['senders'][mask]
+# cut edges that don't connect the MAX_NODES nodes
+mask = (np.isin(input_graphs[0]['receivers'],index)) & (np.isin(input_graphs[0]['senders'],index))
+receivers = input_graphs[0]['receivers'][mask]
+senders = input_graphs[0]['senders'][mask]
+mapping = np.empty(index.max() + 1, dtype=index.dtype)
+mapping[index] = np.arange(input_graphs[0]['nodes'].shape[0])
+input_graphs[0]['receivers'] = mapping[receivers]
+input_graphs[0]['senders'] = mapping[senders]
 input_graphs[0]['edges'] = input_graphs[0]['edges'][mask]
 input_graphs[0]['n_edge'] = np.asarray(input_graphs[0]['edges'].shape[0])
 input_graphs[0]['globals'] = input_graphs[0]['globals']
+
+print('{} nodes and {} edges'.format(input_graphs[0]['nodes'].shape[0], input_graphs[0]['edges'].shape[0]))
 
 with np.load(file_name.replace("INPUT", "TARGET")) as f:
     target_graphs.append(dict(f.items()))
 
 tf.reset_default_graph()
-# make latent dim smaller (32)
-LATENT = 32
+# make latent dim smaller (4)
+LATENT = 4
 encoder = gn.modules.GraphIndependent(
     edge_model_fn=lambda: snt.Sequential([snt.nets.MLP([LATENT, LATENT], activation=tf.nn.relu, activate_final=True)]),
     node_model_fn=lambda: snt.Sequential([snt.nets.MLP([LATENT, LATENT], activation=tf.nn.relu, activate_final=True)]),
