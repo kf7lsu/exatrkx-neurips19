e51ebb934b5c4d612779528cced5e1ef29f71751
Javier M. Duarte <jduarte@ucsd.edu>
2020-09-26 18:59:57 -0700

    update

--- gnn-tracking/heptrkx/nx_graph/model.py
+++ gnn-tracking/heptrkx/nx_graph/model.py
@@ -8,7 +8,7 @@ from graph_nets import utils_tf
 import sonnet as snt
 
 NUM_LAYERS = 2   # Hard-code number of layers in the edge/node/global models.
-LATENT_SIZE = 32 # Hard-code latent layer sizes for demos.
+LATENT_SIZE = 8 # Hard-code latent layer sizes for demos.
 
 
 def make_mlp_model():
