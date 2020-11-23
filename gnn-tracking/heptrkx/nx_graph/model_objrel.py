acff81be9f0ba101816c62040c9ec79d4a1c3dc0
Javier M. Duarte <jduarte@ucsd.edu>
2020-11-23 15:11:48 -0800

    8,8

--- gnn-tracking/heptrkx/nx_graph/model_objrel.py
+++ gnn-tracking/heptrkx/nx_graph/model_objrel.py
@@ -7,19 +7,22 @@ from graph_nets import modules
 from graph_nets import utils_tf
 import sonnet as snt
 
+OBJ_HIDDEN = 8
+REL_HIDDEN = 8
+
 class SegmentClassifier(snt.AbstractModule):
 
   def __init__(self, name="SegmentClassifier"):
     super(SegmentClassifier, self).__init__(name=name)
 
     self._obj_mlp = snt.Sequential([
-      snt.nets.MLP([8, 8, 3],
+      snt.nets.MLP([OBJ_HIDDEN, OBJ_HIDDEN, 3],
                    activation=tf.nn.relu,
                    activate_final=False),
     ])
   
     self._rel_mlp = snt.Sequential([
-      snt.nets.MLP([16, 16, 16, 1],
+      snt.nets.MLP([REL_HIDDEN, REL_HIDDEN, REL_HIDDEN, 1],
                    activation=tf.nn.relu,
                    activate_final=False),
     ])
