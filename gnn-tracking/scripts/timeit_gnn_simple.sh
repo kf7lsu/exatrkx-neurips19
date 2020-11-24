a5f9abe3fa01c5097e45673d91aa2fc8dc4c5283
Javier M. Duarte <jduarte@ucsd.edu>
2020-11-24 09:26:53 -0800

    batch size, cpu

--- gnn-tracking/scripts/timeit_gnn_simple.sh
+++ gnn-tracking/scripts/timeit_gnn_simple.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-SETUP="import setGPU
+BATCHSIZE=1
+SETUP="#import setGPU # 1 gpu
+import os
+os.environ['CUDA_VISIBLE_DEVICES']='' # no gpus
 from graph_nets import utils_np, utils_tf
 from heptrkx import load_yaml
 from heptrkx.nx_graph import prepare
-import os
 import glob
 iteration = -1
-batch_size = 128*10
+batch_size = $BATCHSIZE*10
 from heptrkx.postprocess.evaluate_tf import create_evaluator
 config_file = 'configs/train_edge_classifier_kaggle_share.yaml'
 config = load_yaml(config_file)
@@ -20,10 +22,13 @@ is_digraph = True
 is_bidirection = False
 generate_input_target = prepare.inputs_generator(file_dir, n_train_fraction=0.8)
 I, T = generate_input_target(batch_size,is_train=False)"
-
-for i in {0..10}
+echo $SETUP
+for i in {0..9}
 do
-    j=$((128*i))
-    k=$((128*i+128))
+    j=$((BATCHSIZE*i))
+    k=$((BATCHSIZE*i+BATCHSIZE))
+    echo $i
+    echo $j
+    echo $k
     python -m timeit -s "$SETUP" -n 100 "model(utils_np.data_dicts_to_graphs_tuple(I[$j:$k]),utils_np.data_dicts_to_graphs_tuple(T[$j:$k]),use_digraph=is_digraph, bidirection=is_bidirection)"
 done
